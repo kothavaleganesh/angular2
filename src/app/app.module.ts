f3ad197d61dc641260c39afb252ab6f7e5984fd7
kothavaleganesh <kothavaleganesh@gmail.com>
2018-10-26 08:18:58 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
+import { provideRouter, RouterConfig }  from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+
+const routes: RouterConfig = [
+  {path:'signup',component:SignupComponent
+},{
+  path:'home',component:HomeComponent
+}];
+export const appRouterProviders = [
+  provideRouter(routes)
+];
 export class AppModule { }
